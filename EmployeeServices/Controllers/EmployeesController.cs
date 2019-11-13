b79711086e79ab8ff2853eeba3dd0dc069fe11e9
gopi <karanamgopikrishna@gmail.com>
2019-11-13 22:18:16 +0530

    GET,PUT, PUSH,DELETE tutorial code

--- EmployeeServices/Controllers/EmployeesController.cs
+++ EmployeeServices/Controllers/EmployeesController.cs
@@ -63,5 +63,90 @@ namespace EmployeeServices.Controllers
         }
 
 
+        public HttpResponseMessage Post([FromBody] Employee employee)
+        {
+            try
+            {
+                using (EF_Demo_DBEntities dbContext = new EF_Demo_DBEntities())
+                {
+                    dbContext.Employees.Add(employee);
+                    dbContext.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri + employee.ID.ToString());
+
+                    return message;
+                }
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+
+        public HttpResponseMessage Put(int id, [FromBody]Employee employee )
+        {
+            try
+            {
+                using (EF_Demo_DBEntities dBEntities = new EF_Demo_DBEntities())
+                {
+                    var entity = dBEntities.Employees.FirstOrDefault(e => e.ID == id);
+                    if(entity==null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with ID: " + id.ToString() + " not found to update");
+
+                    }
+                    else
+                    {
+                        entity.FirstName = employee.FirstName;
+                        entity.LastName = employee.LastName;
+                        entity.Gender = employee.Gender;
+                        entity.Salary = employee.Salary;
+
+                        dBEntities.SaveChanges();
+
+
+                        return Request.CreateResponse(HttpStatusCode.OK, entity);
+                    
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+
+
+        public HttpResponseMessage Delete(int id)
+        {
+            using (EF_Demo_DBEntities dBEntities = new EF_Demo_DBEntities())
+            {
+
+                try
+                {
+                    var entity = dBEntities.Employees.FirstOrDefault(e => e.ID == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with ID:" + id.ToString() + " is not found to delete");
+                    }
+                    else
+                    {
+                        dBEntities.Employees.Remove(entity);
+                        dBEntities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+                }
+                catch(Exception ex)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+                }
+                
+            }
+        }
+
+
     }
 }
