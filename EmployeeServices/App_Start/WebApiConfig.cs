9d33c83e66545590bb3461d33c33045d7454139c
gopi <karanamgopikrishna@gmail.com>
2019-11-13 11:31:08 +0530

    myCommit

---
+++ EmployeeServices/App_Start/WebApiConfig.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace EmployeeServices
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            ////Mediatype Formatter with accept headers as only JSON but not XML
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+
+            ////Mediatype Formatter with accept headers as only JSON but not XML
+            config.Formatters.Remove(config.Formatters.JsonFormatter);
+
+
+            ////Mediatype Formatter with accept headers as only html i.e text/html
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+
+            ///Register Custom Formatter
+            config.Formatters.Add(new CustomJsonFormatter());
+
+
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+
+    public class CustomJsonFormatter : JsonMediaTypeFormatter
+    {
+        public CustomJsonFormatter()
+        {
+            this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+        }
+        public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+        {
+            base.SetDefaultContentHeaders(type, headers, mediaType);
+            headers.ContentType = new MediaTypeHeaderValue("application/json");
+        }
+    }
+
+
+
+}
---
+++ EmployeeServices/Controllers/EmployeesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeServices.Models;
+
+namespace EmployeeServices.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        //public IEnumerable<Employee> Get()
+        //{
+        //    using(EmployeeDBContext dbContext =new EmployeeDBContext())
+        //    {
+        //        return dbContext.Employees.ToList();
+        //    }
+        //}
+
+        //public Employee Get(int id)
+        //{
+        //    using(EmployeeDBContext dBContext=new EmployeeDBContext())
+        //    {
+        //        return dBContext.Employees.FirstOrDefault(e => e.ID == id);
+        //    }
+        //}
+
+
+        public IEnumerable<string> Get()
+        {
+            IList<string> formatters = new List<string>();
+            foreach(var item in GlobalConfiguration.Configuration.Formatters)
+            {
+                formatters.Add(item.ToString());
+            }
+            return formatters.AsEnumerable<string>();
+        }
+    }
+}
